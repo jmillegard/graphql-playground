8a30c4fe6ad1fac8ca455e2d8d04fb26ea80ce1f
Johannes Millegård <nour.johannes@gmail.com>
2017-10-10 10:54:15 +0200

    initial

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const express = require('express')
+const http = require('http')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql');
+const app = express()
+const middleware = require('graphql-voyager/middleware');
+
+
+let schema = buildSchema(`
+  type Query {
+    post: Post,
+    comments: [Comment]
+  }
+  
+  type Post {
+    id: String!,
+    title: String,
+    body: String
+  }
+  
+  type Comment {
+    id: String!,
+    comment: String,
+    author: String
+  }
+`)
+
+const root = {
+  post: () => {
+    return {
+      id: '1',
+      title: 'Thoughts from NordicAPIs',
+      body: 'Hello from strockholm'
+    }
+  },
+  comments: () => [
+    {id: '1', comment: 'Gott kaffe', author: 'Johannes'},
+  ]
+}
+
+app.use('/voyager', middleware.express({ endpointUrl: '/graphql' }));
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+http.createServer(app).listen(4001, () => {
+  console.log('app started...')
+})
+
+
+
